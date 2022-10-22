6bf4256daad90b5b29668ae234ec014ab7c4e817
Nikita Nechaev <nnsf0sker@gmail.com>
2022-10-22 17:17:02 +0300

    is_camel_case problem was stated

--- is_camel_case.py
+++ is_camel_case.py
@@ -1,8 +1,8 @@
-def is_camel_case(string: str, allow_one_word: bool = True) -> bool:
+def is_camel_case(variable_name: str, allow_one_word: bool = True) -> bool:
     """
     Determine if the string spelled in CamelCase or not
     More info: https://en.wikipedia.org/wiki/Camel_case
-    :param string: Sequence of symbols to check on CamelCase
+    :param variable_name: Sequence of symbols to check on CamelCase
     :param allow_one_word: Flag to determine one-word string as correct CamelCase string
     """
     pass
@@ -13,12 +13,18 @@ if __name__ == "__main__":
     assert is_camel_case("Camel")
     assert is_camel_case("CamelCase")
     assert is_camel_case("CamelCase", allow_one_word=False)
+    assert is_camel_case("CamelCase")
+    assert is_camel_case("PowerPoint")
+    assert is_camel_case("JetBrains")
+    assert is_camel_case("PyCharm")
+    assert is_camel_case("PyCharm2022")
     assert is_camel_case("ThreeWordsTest")
     assert is_camel_case("ALittleBitLongerExpression")
     assert is_camel_case("ParseDBMXML")
     assert is_camel_case("SQLServer")
     assert is_camel_case("ParseDbmXml")
     assert is_camel_case("SqlServer")
+    assert is_camel_case("NewYork2022")
 
     # Negative cases
     assert not is_camel_case("camel")
@@ -28,6 +34,7 @@ if __name__ == "__main__":
     assert not is_camel_case("VeryLongExpressionWithOneSmallTail_")
     assert not is_camel_case("WhatAboutQuestionMark?")
     assert not is_camel_case("AndWhatAboutSomeSpaces ")
+    assert not is_camel_case("Microsoft PowerPoint")
     assert not is_camel_case("ByTheWay,whatAboutCommas")
     assert not is_camel_case("ByTheWay,WhatAboutCommas")
     assert not is_camel_case("wtf")
